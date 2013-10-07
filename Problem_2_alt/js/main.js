ccc03a285599810f9d886770d2130c2a4d57da59
jsegal205 <Jim@Jim-Segals-MacBook.local>
2013-10-07 15:29:17 -0400

    added work steps.

--- Problem_2_alt/js/main.js
+++ Problem_2_alt/js/main.js
@@ -11,17 +11,23 @@
 			if (fib[fib.length - 1] > 4000000){
 				run = false;
 			}
-			console.log(fib[fib.length -1]);
 		}
 		while(run);
 
-		for(var i = 0; i<fib.length; i++){
-
-			if ((fib[i] % 2 == 0) && (fib[i]<4000000)){
-				console.log(fib[i]);
+		var sum = 0;
+		var work = '';
+		var evenWork = '';
+		for(var i = 0; i < fib.length; i++){
+			if ((fib[i] % 2 == 0) && (fib[i] < 4000000)){
+				sum += fib[i];
+				evenWork += fib[i] + ' + ';
 			}
+			work += fib[i] + ' + ';
 		}
-
+		document.getElementById('Work').innerHTML = '<p>Fibinacci numbers below 4 million:</p><p>' 
+												+ work.substring(work.length-2,2) + '</p><p>Even Fibinacci numbers below 4 million:</p>' 
+												+ evenWork.substring(evenWork.length-2,2);
+		document.getElementById('Answer').innerHTML = sum;
 	}
 
 	doWork();	
