c08798f08eec53d14ba0c2be5f6425239c2c8ab6
jsegal205 <Jim@Jim-Segals-MacBook.local>
2013-10-07 17:50:29 -0400

    updated substring. Changed array to start with 1 rather than 0. Updated for loop to start at 1 to reduce dupe values in work.

--- Problem_2_alt/js/main.js
+++ Problem_2_alt/js/main.js
@@ -6,7 +6,7 @@
 		var sum = 0;
 		var work = '';
 		var evenWork = '';
-		for(var i = 0; i < fib.length; i++) {
+		for(var i = 1; i < fib.length; i++) {
 			if (fib[i] < 4000000) {
 				work += fib[i] + ', ';
 				if (fib[i] % 2 == 0){
@@ -17,19 +17,19 @@
 		}
 
 		document.getElementById('Work').innerHTML = '<p>Fibinacci numbers below 4 million:</p><p>' 
-												+ work.substring(work.length-2,2) + '</p><p>Even Fibinacci numbers below 4 million:</p>' 
-												+ evenWork.substring(evenWork.length-2,2);
+												+ work.substring(0, work.length - 2) + '</p><p>Even Fibinacci numbers below 4 million:</p>' 
+												+ evenWork.substring(0, evenWork.length - 2);
 		document.getElementById('Answer').innerHTML = sum;
 	}
 
 	function loadFib(){
 		var run = true;
 		var fib = [];
-		fib.push(0);
+		fib.push(1);
 		fib.push(1);
 		
 		do{
-			fib.push(fib[fib.length - 1] + fib[fib.length-2]);
+			fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
 
 			if (fib[fib.length - 1] > 4000000){
 				run = false;
