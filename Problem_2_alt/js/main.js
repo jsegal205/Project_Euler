7f8a267e176e4eb48a4c4f57ca2cf63e4af5dd77
jsegal205 <Jim@Jim-Segals-MacBook.local>
2013-10-07 17:40:37 -0400

    removed local variable.

--- Problem_2_alt/js/main.js
+++ Problem_2_alt/js/main.js
@@ -15,6 +15,7 @@
 				}
 			}
 		}
+
 		document.getElementById('Work').innerHTML = '<p>Fibinacci numbers below 4 million:</p><p>' 
 												+ work.substring(work.length-2,2) + '</p><p>Even Fibinacci numbers below 4 million:</p>' 
 												+ evenWork.substring(evenWork.length-2,2);
@@ -26,9 +27,9 @@
 		var fib = [];
 		fib.push(0);
 		fib.push(1);
+		
 		do{
-			var fibNum = fib[fib.length - 1];
-			fib.push(fibNum + fib[fib.length-2]);
+			fib.push(fib[fib.length - 1] + fib[fib.length-2]);
 
 			if (fib[fib.length - 1] > 4000000){
 				run = false;
