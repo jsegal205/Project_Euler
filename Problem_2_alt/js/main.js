512b507b9d5dc27c6a2c69dedc88c7dd8435f8bb
jsegal205 <Jim@Jim-Segals-MacBook.local>
2013-10-07 17:28:45 -0400

    moved if statment to correctly show numbers under 4million

--- Problem_2_alt/js/main.js
+++ Problem_2_alt/js/main.js
@@ -17,12 +17,14 @@
 		var sum = 0;
 		var work = '';
 		var evenWork = '';
-		for(var i = 0; i < fib.length; i++){
-			if ((fib[i] % 2 == 0) && (fib[i] < 4000000)){
-				sum += fib[i];
-				evenWork += fib[i] + ', ';
+		for(var i = 0; i < fib.length; i++) {
+			if (fib[i] < 4000000) {
+				work += fib[i] + ', ';
+				if (fib[i] % 2 == 0){
+					sum += fib[i];
+					evenWork += fib[i] + ', ';
+				}
 			}
-			work += fib[i] + ', ';
 		}
 		document.getElementById('Work').innerHTML = '<p>Fibinacci numbers below 4 million:</p><p>' 
 												+ work.substring(work.length-2,2) + '</p><p>Even Fibinacci numbers below 4 million:</p>' 
